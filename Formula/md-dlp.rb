2d7b33af8bcef42eb53aab29fa07ba20d1f996c5
markshawn2020 <shawninjuly@gmail.com>
2025-02-01 23:12:27 +0800

    fix version

--- Formula/md-dlp.rb
+++ Formula/md-dlp.rb
@@ -7,22 +7,7 @@ class MdDlp < Formula
   sha256 "e246ab9ca0c2e42f6939226d7793025e164f9b68a113f0b1d0929d35b027b7c8"
   license "MIT"
 
-  depends_on "python@3.11"
-
-  resource "beautifulsoup4" do
-    url "https://github.com/markshawn2020/md-dlp/archive/v0.0.2.tar.gz"
-    sha256 "e246ab9ca0c2e42f6939226d7793025e164f9b68a113f0b1d0929d35b027b7c8"
-  end
-
-  resource "requests" do
-    url "https://github.com/markshawn2020/md-dlp/archive/v0.0.2.tar.gz"
-    sha256 "e246ab9ca0c2e42f6939226d7793025e164f9b68a113f0b1d0929d35b027b7c8"
-  end
-
-  resource "soupsieve" do
-    url "https://github.com/markshawn2020/md-dlp/archive/v0.0.2.tar.gz"
-    sha256 "e246ab9ca0c2e42f6939226d7793025e164f9b68a113f0b1d0929d35b027b7c8"
-  end
+  depends_on "python"
 
   def install
     virtualenv_install_with_resources
